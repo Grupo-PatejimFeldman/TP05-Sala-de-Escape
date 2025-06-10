6d989fdc19cdc3809b488c4e43fce5c722eaa742
MAXIMO URIEL PATEJIM <49090825@ort>
2025-06-10 12:14:52 -0300

    creacion inicio views

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddSession();
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
@@ -20,6 +22,7 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+app.UseSession();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
