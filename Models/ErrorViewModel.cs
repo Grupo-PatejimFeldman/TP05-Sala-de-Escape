e360d23455acc3d4df0f39c25063e8aec68254a4
MAXIMO URIEL PATEJIM <49090825@ort>
2025-06-10 11:23:54 -0300

    inicio

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP05_Patejim_Feldman.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
