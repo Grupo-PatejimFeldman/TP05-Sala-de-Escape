be6c1018a05c66b13a8ecc8d421dd33a58447e77
MAXIMO URIEL PATEJIM <49090825@ort>
2025-06-24 12:12:48 -0300

    en proceso

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -21,6 +21,12 @@ public class HomeController : Controller
         return View("Index");
     }
 
+    public IActionResult Registro(){
+        Juego juego = Objeto.StringToObject<Juego>(HttpContext.Session.GetString("Juego"));
+        HttpContext.Session.SetString("Juego", Objeto.ObjectToString(juego));
+        return View("Registro");
+    }
+    
     public IActionResult Volver(){
         Juego juego = Objeto.StringToObject<Juego>(HttpContext.Session.GetString("Juego"));
         HttpContext.Session.SetString("Juego", Objeto.ObjectToString(juego));
@@ -45,28 +51,27 @@ public class HomeController : Controller
 
     /* EMPIEZA EL JUEGO */
 
-    public IActionResult Comienzo(){
+    public IActionResult Comienzo(string Nombre, string Id){
         Juego juego = Objeto.StringToObject<Juego>(HttpContext.Session.GetString("Juego"));
         HttpContext.Session.SetString("Juego", Objeto.ObjectToString(juego));
-        ViewBag.Vidas = juego.vidas;
         return View("Sala1");
     }
 
     public IActionResult Respuesta(string respuesta){
         Juego juego = Objeto.StringToObject<Juego>(HttpContext.Session.GetString("Juego"));
-        HttpContext.Session.SetString("Juego", Objeto.ObjectToString(juego));
         ViewBag.Vidas = juego.vidas;
         int result = juego.Respuesta(respuesta);
+        HttpContext.Session.SetString("Juego", Objeto.ObjectToString(juego));
         if (result == 0){
             return View("Perdedor"); /* HACER LA VIEW DE PERDEDOR */
         }else if(result == 1){
             return View("VidaMenos");
         }
         switch(juego.habitacion){
-            case 0: return View("Sala1"); break;
-            case 1: return View("Sala2"); break;
-            case 2: return View("Sala3"); break;
-            case 3: return View("Sala4"); break;
+            case 0: return View("Sala1");
+            case 1: return View("Sala2");
+            case 2: return View("Sala3");
+            case 3: return View("Sala4");
         }
         return View();
     }
@@ -75,10 +80,10 @@ public class HomeController : Controller
         Juego juego = Objeto.StringToObject<Juego>(HttpContext.Session.GetString("Juego"));
         HttpContext.Session.SetString("Juego", Objeto.ObjectToString(juego));
         switch(juego.habitacion){
-            case 0: return View("Sala1"); break;
-            case 1: return View("Sala2"); break;
-            case 2: return View("Sala3"); break;
-            case 3: return View("Sala4"); break;
+            case 0: return View("Sala1");
+            case 1: return View("Sala2");
+            case 2: return View("Sala3");
+            case 3: return View("Sala4");
         }
         return View();
     }
